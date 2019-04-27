c321b31e83bf89590b5d438fa1faa2cd532a2fc4
Gerardo Reyes Preciado <gerardo.reyes.preciado@hotmail.com>
2019-04-27 10:32:40 -0500

    test

--- smay_reprint_ticket/static/src/js/pos.js
+++ smay_reprint_ticket/static/src/js/pos.js
@@ -7,6 +7,8 @@ odoo.define('smay_reprint_ticket.smay_reprint_ticket', function(require){
     var Model = require('web.DataModel');
     var core = require('web.core');
     var QWeb = core.qweb;
+	
+	
 
 
     chrome.OrderSelectorWidget.include({
