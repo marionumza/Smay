7a2f8ed14ac39a191c3f823e3ee828938a86e680
Gerardo Reyes Preciado <gerardo.reyes.preciado@hotmail.com>
2019-04-30 09:44:08 -0500

    20170430_A

--- smay_window_product/static/src/js/pos.js
+++ smay_window_product/static/src/js/pos.js
@@ -4,6 +4,7 @@ odoo.define('smay_window_product.smay_window_product', function(require){
     //var chrome = require('point_of_sale.chrome');
     var gui = require('point_of_sale.gui');
     var popups = require('point_of_sale.popups');
+    var screens = require('point_of_sale.screens')
     //var Model = require('web.DataModel');
     //var core = require('web.core');
     //var QWeb = core.qweb;
@@ -16,6 +17,10 @@ odoo.define('smay_window_product.smay_window_product', function(require){
 	        var self = this;
 	        self._super(options);
 
+	        self.$('.priceProduct').on('click',function(){
+	            console.log('clic al producto')
+	        });
+
             self.$('#cancel').on('click',function(){
                 self.gui.close_popup();
             });
@@ -24,5 +29,27 @@ odoo.define('smay_window_product.smay_window_product', function(require){
 	});
 
 	gui.define_popup({name:'InformationProductPopUp',widget: InformationProductPopUp});
+
+	screens.ProductScreenWidget.include({
+
+
+        click_product: function(product) {
+        var self = this;
+        console.log('Entro al clic product');
+        console.log(product)
+        self.gui.show_popup('InformationProductPopUp',{
+            'title': product.name,
+            'id': product.id,
+            'url_image': "http://192.168.5.216:8069/web/image?model=product.product&field=image&id="+product.id,
+        });
+        /*
+           if(product.to_weight && this.pos.config.iface_electronic_scale){
+               this.gui.show_screen('scale',{product: product});
+           }else{
+               this.pos.get_order().add_product(product);
+           }
+           */
+        },
+	});
 
  });
